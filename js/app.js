b6590582ce0aff0f380e5a79b55bf197ef607ef2
Jim Heising <jheising@redfoundry.com>
2014-05-22 01:42:40 -0700

    vimeo fixes

--- js/app.js
+++ js/app.js
@@ -6,76 +6,67 @@ var videoScreenHeight = 1152;
 var videoScreenWidth = 495;
 var grainStage;
 
-function loadVimeoVideo(videoID)
-{
-    $("#videoScreen").empty();
+function loadVimeoVideo(videoID) {
 
     var url = "http://player.vimeo.com/video/" + videoID;
 
-    var iframe = $('<iframe id="videoSource" src="' + url + '?api=1&title=0&byline=0"></iframe>');
+    var iframe = $("#videoSource").attr("src", url + "?api=1&title=0&byline=0");
 
-    $("#videoScreen").append(iframe);
-
-    iframe.load(function(){
+    iframe.load(function () {
 
         var contentWindow = this.contentWindow;
 
         var message = {
-            method : "setVolume",
-            value : 0
+            method: "setVolume",
+            value: 0
         };
         contentWindow.postMessage(JSON.stringify(message), url);
 
         message = {
-            method : "setLoop",
-            value : 1
+            method: "setLoop",
+            value: 1
         };
         contentWindow.postMessage(JSON.stringify(message), url);
 
         message = {
-            method : "play"
+            method: "play"
         };
         contentWindow.postMessage(JSON.stringify(message), url);
     });
 }
 
 // Handle messages received from the vimeo
-if (window.addEventListener){
+if (window.addEventListener)
+{
     window.addEventListener('message', onVimeoMessageReceived, false);
 }
-else {
+else
+{
     window.attachEvent('onmessage', onVimeoMessageReceived, false);
 }
+
 function onVimeoMessageReceived(e) {
     var data = JSON.parse(e.data);
 
-    /*switch (data.event) {
-        case 'ready':
-            onReady();
-            break;
-
-        case 'playProgress':
-            onPlayProgress(data.data);
-            break;
-
-        case 'pause':
-            onPause();
-            break;
+    switch (data.event) {
+        //case 'ready':
+        //    onReady();
+        //    break;
 
         case 'finish':
             onFinish();
             break;
-    }*/
+    }
+
     console.log(data);
 }
 
-$(function() {
+$(function () {
 
     var grainCanvas = document.getElementById("grain");
     var canvas = document.getElementById("mainCanvas");
 
-    function calculateAspectFillScale(targetWidth, targetHeight, nativeWidth, nativeHeight)
-    {
+    function calculateAspectFillScale(targetWidth, targetHeight, nativeWidth, nativeHeight) {
         // Calculate resize ratios for resizing
         var ratioW = targetWidth / nativeWidth;
         var ratioH = targetHeight / nativeHeight;
@@ -86,23 +77,20 @@ $(function() {
         return ratio;
     }
 
-    function spinnerFlight1()
-    {
+    function spinnerFlight1() {
         layers.spinner.x = 1400;
         layers.spinner.y = 650;
         //layers.spinner.alpha = 0;
 
-        createjs.Tween.get(layers.spinner, {loop:true}).to({x:980,y:680}, 2000).to({alpha:0});
+        createjs.Tween.get(layers.spinner, {loop: true}).to({x: 980, y: 680}, 2000).to({alpha: 0});
 
     }
 
-    function resize()
-    {
+    function resize() {
         grainCanvas.width = canvas.width = window.innerWidth;
         grainCanvas.height = canvas.height = window.innerHeight;
 
-        if(images.background)
-        {
+        if (images.background) {
             var scaleRatio = calculateAspectFillScale(canvas.width, canvas.height, images.background.image.width, images.background.image.height);
 
             layers.background.scaleX = layers.background.scaleY = scaleRatio;
@@ -115,34 +103,30 @@ $(function() {
 
             $("#videoContainer").width(572);
             $("#videoContainer").height(images.background.image.height);
-            $("#videoContainer").css({"-webkit-transform" : "scale(" + scaleRatio + "," + scaleRatio + ")"});
+            $("#videoContainer").css({"-webkit-transform": "scale(" + scaleRatio + "," + scaleRatio + ")"});
         }
 
-        if(layers.filmGrain)
-        {
+        if (layers.filmGrain) {
             // Work on our film grain
             layers.filmGrain.graphics.clear();
-            layers.filmGrain.graphics.beginBitmapFill(images.filmGrain,'repeat').drawRect(0, 0, canvas.width + 1000, canvas.height + 1000);
+            layers.filmGrain.graphics.beginBitmapFill(images.filmGrain, 'repeat').drawRect(0, 0, canvas.width + 1000, canvas.height + 1000);
 
-            layers.filmGrain.cache(0,0, canvas.width + 1000, canvas.height + 1000);
+            layers.filmGrain.cache(0, 0, canvas.width + 1000, canvas.height + 1000);
         }
 
-        if(layers.twinkle)
-        {
+        if (layers.twinkle) {
             // Work on our film grain
             layers.twinkle.graphics.clear();
-            layers.twinkle.graphics.beginBitmapFill(images.twinkle,'repeat').drawRect(0, 0, canvas.width + 1000, canvas.height + 1000);
+            layers.twinkle.graphics.beginBitmapFill(images.twinkle, 'repeat').drawRect(0, 0, canvas.width + 1000, canvas.height + 1000);
 
             layers.twinkle.cache(0, 0, canvas.width + 1000, canvas.height + 1000);
         }
 
 
-
         stage.update();
     }
 
-    function init()
-    {
+    function init() {
         $("#controls").hide();
 
         stage = new createjs.Stage("mainCanvas");
@@ -150,24 +134,24 @@ $(function() {
 
         loadQueue = new createjs.LoadQueue();
 
-        if(location.protocol == "file:")
+        if (location.protocol == "file:")
             loadQueue.setUseXHR(false);
 
         loadQueue.on("complete", finishLoad, this);
         loadQueue.loadManifest([
-            {id:"background", src:"img/background.jpg"},
-            {id:"backgroundMask", src:"img/background-mask.png"},
-            {id:"filmGrain", src:"img/noise.png"},
-            {id:"twinkle", src:"img/twinkling.png"},
-            {id:"blinker1", src:"img/blinker1.png"},
-            {id:"blinker2", src:"img/blinker2.png"},
-            {id:"blinker3", src:"img/blinker3.png"},
-            {id:"lightning", src:"img/lightning.png"},
-            {id:"smoke", src:"img/smoke.png"},
-            {id:"smokeOverlay", src:"img/smokeOverlay.png"},
-            {id:"neon1", src:"img/neon1.png"},
-            {id:"neon2", src:"img/neon2.png"},
-            {id:"spinner", src:"img/spinner.png"},
+            {id: "background", src: "img/background.jpg"},
+            {id: "backgroundMask", src: "img/background-mask.png"},
+            {id: "filmGrain", src: "img/noise.png"},
+            {id: "twinkle", src: "img/twinkling.png"},
+            {id: "blinker1", src: "img/blinker1.png"},
+            {id: "blinker2", src: "img/blinker2.png"},
+            {id: "blinker3", src: "img/blinker3.png"},
+            {id: "lightning", src: "img/lightning.png"},
+            {id: "smoke", src: "img/smoke.png"},
+            {id: "smokeOverlay", src: "img/smokeOverlay.png"},
+            {id: "neon1", src: "img/neon1.png"},
+            {id: "neon2", src: "img/neon2.png"},
+            {id: "spinner", src: "img/spinner.png"},
         ]);
 
         $("#ambientAudio").on("canplay", finishLoad);
@@ -177,28 +161,24 @@ $(function() {
 
     var filesCompleted = false;
     var soundsCompleted = false;
-    function finishLoad(event)
-    {
-        if(event.type == "canplay")
-        {
+
+    function finishLoad(event) {
+        if (event.type == "canplay") {
             soundsCompleted = true;
         }
-        else
-        {
+        else {
             filesCompleted = true;
         }
 
-        if(filesCompleted && soundsCompleted)
-        {
+        if (filesCompleted && soundsCompleted) {
             buildInterface();
         }
     }
 
-    function buildInterface()
-    {
+    function buildInterface() {
         resize();
 
-        $(".bubble-loader").animate({opacity:0.0},  1000);
+        $(".bubble-loader").animate({opacity: 0.0}, 1000);
 
         layers.background = new createjs.Container();
         layers.backgroundOverlay = new createjs.Container();
@@ -230,8 +210,8 @@ $(function() {
         grainStage.addChild(layers.filmGrain);
 
         // Work on our background
-        layers.background.setBounds(0,0, images.background.image.width, images.background.image.height);
-        layers.backgroundOverlay.setBounds(0,0, images.background.image.width, images.background.image.height);
+        layers.background.setBounds(0, 0, images.background.image.width, images.background.image.height);
+        layers.backgroundOverlay.setBounds(0, 0, images.background.image.width, images.background.image.height);
 
         layers.background.addChild(images.background);
 
@@ -247,21 +227,21 @@ $(function() {
         // Setup our smoke
 
         layers.smoke.graphics.clear();
-        layers.smoke.graphics.beginBitmapFill(images.smoke,'repeat').drawRect(0, 0, images.smoke.width, images.smoke.height * 2);
+        layers.smoke.graphics.beginBitmapFill(images.smoke, 'repeat').drawRect(0, 0, images.smoke.width, images.smoke.height * 2);
 
         layers.smoke.alpha = 0.5;
         layers.smokeOverlay.x = 737;
         layers.smokeOverlay.y = 277;
         var smokeMask = new createjs.Shape();
-        smokeMask.graphics.beginFill("#000").drawRect(layers.smokeOverlay.x,layers.smokeOverlay.y,images.smokeOverlay.image.width, images.smokeOverlay.image.height);
+        smokeMask.graphics.beginFill("#000").drawRect(layers.smokeOverlay.x, layers.smokeOverlay.y, images.smokeOverlay.image.width, images.smokeOverlay.image.height);
         layers.smokeOverlay.mask = smokeMask;
-        layers.smokeOverlay.setBounds(0,0,52,53);
+        layers.smokeOverlay.setBounds(0, 0, 52, 53);
         layers.smokeOverlay.addChild(layers.smoke);
         layers.smokeOverlay.addChild(images.smokeOverlay);
 
         layers.backgroundOverlay.addChild(layers.smokeOverlay);
 
-        createjs.Tween.get(layers.smoke, {override:true, loop:true}).to({y:-images.smoke.height}, 15000);
+        createjs.Tween.get(layers.smoke, {override: true, loop: true}).to({y: -images.smoke.height}, 15000);
 
         // Setup our blinkers and neons
         layers.backgroundOverlay.addChild(images.blinker1);
@@ -299,7 +279,7 @@ $(function() {
         createjs.Ticker.setFPS(24);
         createjs.Ticker.addEventListener("tick", tick);
 
-        $("#loadingIndicator").fadeOut(5000, function(){
+        $("#loadingIndicator").fadeOut(5000, function () {
             $(this).remove();
         });
 
@@ -309,39 +289,32 @@ $(function() {
     var lightningOn = false;
     var lightningLength = getRandom(250, 1000);
 
-    function tick()
-    {
+    function tick() {
         layers.twinkle.x = getRandom(-500, 500) - 500;
         layers.twinkle.y = getRandom(-500, 500) - 500;
 
         processNeon(images.neon1);
 
         // Do our lightning
-        if(!lightningOn)
-        {
+        if (!lightningOn) {
             lightningLength--;
 
-            if(lightningLength <= 0)
-            {
+            if (lightningLength <= 0) {
                 lightningOn = true;
                 lightningLength = getRandom(25, 50);
             }
         }
-        else
-        {
-            if(getRandom(0,500) < 25)
-            {
+        else {
+            if (getRandom(0, 500) < 25) {
                 images.lighting.alpha = 0;
             }
-            else
-            {
+            else {
                 images.lighting.alpha = 1;
             }
 
             lightningLength--;
 
-            if(lightningLength <= 0)
-            {
+            if (lightningLength <= 0) {
                 lightningLength = getRandom(250, 1000);
                 images.lighting.alpha = 0;
                 lightningOn = false;
@@ -377,34 +350,29 @@ $(function() {
     });
 });
 
-function getRandom (min, max) {
+function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function processNeon(target)
-{
-    if(getRandom(0,750) < 25)
-    {
+function processNeon(target) {
+    if (getRandom(0, 750) < 25) {
         target.alpha = 0.0;
     }
-    else
-    {
+    else {
         target.alpha = 1.0;
     }
 }
 
-function makeBlink(target, onTime, offTime, fadeTime)
-{
+function makeBlink(target, onTime, offTime, fadeTime) {
     target.alpha = 0.0;
 
-    function beginAnimation()
-    {
+    function beginAnimation() {
         target.alpha = 0.0;
-        createjs.Tween.get(target, {override:true, loop:true}).to({alpha:1.0}, fadeTime).wait(onTime).to({alpha:0.0}, fadeTime).wait(offTime);
+        createjs.Tween.get(target, {override: true, loop: true}).to({alpha: 1.0}, fadeTime).wait(onTime).to({alpha: 0.0}, fadeTime).wait(offTime);
     }
 
     // Choose a random start time
-    createjs.Tween.get(target, {override:true}).wait(getRandom(0, offTime)).call(beginAnimation);
+    createjs.Tween.get(target, {override: true}).wait(getRandom(0, offTime)).call(beginAnimation);
 }
 
 /*!
@@ -412,5 +380,40 @@ function makeBlink(target, onTime, offTime, fadeTime)
  * v1.2.0 - 2014-04-29
  * (c) Sindre Sorhus; MIT License
  */
-!function(){"use strict";var a="undefined"!=typeof module&&module.exports,b="undefined"!=typeof Element&&"ALLOW_KEYBOARD_INPUT"in Element,c=function(){for(var a,b,c=[["requestFullscreen","exitFullscreen","fullscreenElement","fullscreenEnabled","fullscreenchange","fullscreenerror"],["webkitRequestFullscreen","webkitExitFullscreen","webkitFullscreenElement","webkitFullscreenEnabled","webkitfullscreenchange","webkitfullscreenerror"],["webkitRequestFullScreen","webkitCancelFullScreen","webkitCurrentFullScreenElement","webkitCancelFullScreen","webkitfullscreenchange","webkitfullscreenerror"],["mozRequestFullScreen","mozCancelFullScreen","mozFullScreenElement","mozFullScreenEnabled","mozfullscreenchange","mozfullscreenerror"],["msRequestFullscreen","msExitFullscreen","msFullscreenElement","msFullscreenEnabled","MSFullscreenChange","MSFullscreenError"]],d=0,e=c.length,f={};e>d;d++)if(a=c[d],a&&a[1]in document){for(d=0,b=a.length;b>d;d++)f[c[0][d]]=a[d];return f}return!1}(),d={request:function(a){var d=c.requestFullscreen;a=a||document.documentElement,/5\.1[\.\d]* Safari/.test(navigator.userAgent)?a[d]():a[d](b&&Element.ALLOW_KEYBOARD_INPUT)},exit:function(){document[c.exitFullscreen]()},toggle:function(a){this.isFullscreen?this.exit():this.request(a)},onchange:function(){},onerror:function(){},raw:c};return c?(Object.defineProperties(d,{isFullscreen:{get:function(){return!!document[c.fullscreenElement]}},element:{enumerable:!0,get:function(){return document[c.fullscreenElement]}},enabled:{enumerable:!0,get:function(){return!!document[c.fullscreenEnabled]}}}),document.addEventListener(c.fullscreenchange,function(a){d.onchange.call(d,a)}),document.addEventListener(c.fullscreenerror,function(a){d.onerror.call(d,a)}),void(a?module.exports=d:window.screenfull=d)):void(a?module.exports=!1:window.screenfull=!1)}();
+!function () {
+    "use strict";
+    var a = "undefined" != typeof module && module.exports, b = "undefined" != typeof Element && "ALLOW_KEYBOARD_INPUT"in Element, c = function () {
+        for (var a, b, c = [
+            ["requestFullscreen", "exitFullscreen", "fullscreenElement", "fullscreenEnabled", "fullscreenchange", "fullscreenerror"],
+            ["webkitRequestFullscreen", "webkitExitFullscreen", "webkitFullscreenElement", "webkitFullscreenEnabled", "webkitfullscreenchange", "webkitfullscreenerror"],
+            ["webkitRequestFullScreen", "webkitCancelFullScreen", "webkitCurrentFullScreenElement", "webkitCancelFullScreen", "webkitfullscreenchange", "webkitfullscreenerror"],
+            ["mozRequestFullScreen", "mozCancelFullScreen", "mozFullScreenElement", "mozFullScreenEnabled", "mozfullscreenchange", "mozfullscreenerror"],
+            ["msRequestFullscreen", "msExitFullscreen", "msFullscreenElement", "msFullscreenEnabled", "MSFullscreenChange", "MSFullscreenError"]
+        ], d = 0, e = c.length, f = {}; e > d; d++)if (a = c[d], a && a[1]in document) {
+            for (d = 0, b = a.length; b > d; d++)f[c[0][d]] = a[d];
+            return f
+        }
+        return!1
+    }(), d = {request: function (a) {
+        var d = c.requestFullscreen;
+        a = a || document.documentElement, /5\.1[\.\d]* Safari/.test(navigator.userAgent) ? a[d]() : a[d](b && Element.ALLOW_KEYBOARD_INPUT)
+    }, exit: function () {
+        document[c.exitFullscreen]()
+    }, toggle: function (a) {
+        this.isFullscreen ? this.exit() : this.request(a)
+    }, onchange: function () {
+    }, onerror: function () {
+    }, raw: c};
+    return c ? (Object.defineProperties(d, {isFullscreen: {get: function () {
+        return!!document[c.fullscreenElement]
+    }}, element: {enumerable: !0, get: function () {
+        return document[c.fullscreenElement]
+    }}, enabled: {enumerable: !0, get: function () {
+        return!!document[c.fullscreenEnabled]
+    }}}), document.addEventListener(c.fullscreenchange, function (a) {
+        d.onchange.call(d, a)
+    }), document.addEventListener(c.fullscreenerror, function (a) {
+        d.onerror.call(d, a)
+    }), void(a ? module.exports = d : window.screenfull = d)) : void(a ? module.exports = !1 : window.screenfull = !1)
+}();
 
